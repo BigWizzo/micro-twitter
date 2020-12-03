90a1e6e92f8b6d2972acd1ddb4ce89091dafae2b
bigwizzo <wnnwillard@gmail.com>
2020-12-03 22:47:47 +0200

    Add AddFieldsToUsers migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_03_201234) do
+ActiveRecord::Schema.define(version: 2020_12_03_204129) do
 
   create_table "tweets", force: :cascade do |t|
     t.text "tweet"
